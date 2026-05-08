f23089e94ce1dcf0d9418ba90d004b444af18dc7
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-08 08:32:43 +0530

    register user routes in server entry point

--- ExamNotesAi/Server/index.js
+++ ExamNotesAi/Server/index.js
@@ -4,6 +4,7 @@ import connectDb from './utils/connectDb.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 })
 
 app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
