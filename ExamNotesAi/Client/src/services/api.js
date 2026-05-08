d191d1563ac3e782e1a38b03669aae8d5d204a60
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-08 08:35:42 +0530

    create api service to fetch current user on app load

---
+++ ExamNotesAi/Client/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { serverUrl } from "../App"
+
+export const getCurrentUser = async () => {
+    try{    
+        const result =await axios.get(serverUrl + "/api/user/currentuser", { withCredentials: true });
+        console.log(result.data);
+    }
+    catch(error){
+        console.error( error);
+    }
+}
