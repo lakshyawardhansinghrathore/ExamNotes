a0bb0a14b1cdbadb960620aaeedef1cc1ee59cf4
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-09 13:05:45 +0530

    set up Redux store and protect routes

--- ExamNotesAi/Client/src/App.jsx
+++ ExamNotesAi/Client/src/App.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import { getCurrentUser } from './services/api'
+import { useDispatch, useSelector } from 'react-redux'
+ 
 
-// 1. IMPORT your API function here:
-import { getCurrentUser } from './services/api' 
-
-// 2. CHANGE the port to match your backend (5000 instead of 8000)
 export const serverUrl = "http://localhost:8000"
 
 function App() {
+  const dispatch = useDispatch();   
   useEffect(() => {
-    getCurrentUser()
-  }, [])
+    getCurrentUser(dispatch)
+  }, [dispatch])
+
+  const { userData } = useSelector((state) => state.user);
 
   return (
     <>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/auth' element={<Auth/>}/>
+      <Route path='/' element={userData ? <Home/> : <Navigate to="/auth" replace />} />
+      <Route path='/auth' element={userData ? <Navigate to="/" replace /> : <Auth />} />
     </Routes>
     </>
   )
