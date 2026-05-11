c8ce2f8b2b04ca1c69eb55fd64a068d55c005fc5
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-11 10:12:22 +0530

    Implement homepage, navbar/footer components, and protected routes

--- ExamNotesAi/Client/src/App.jsx
+++ ExamNotesAi/Client/src/App.jsx
@@ -4,7 +4,9 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import { getCurrentUser } from './services/api'
 import { useDispatch, useSelector } from 'react-redux'
- 
+ import History from './pages/History'
+import Notes from './pages/Notes'
+import Pricing from './pages/Pricing'
 
 export const serverUrl = "http://localhost:8000"
 
@@ -21,6 +23,9 @@ function App() {
     <Routes>
       <Route path='/' element={userData ? <Home/> : <Navigate to="/auth" replace />} />
       <Route path='/auth' element={userData ? <Navigate to="/" replace /> : <Auth />} />
+      <Route path='/history' element={userData ? <History/> : <Navigate to="/auth" replace />} />
+      <Route path='/notes' element={userData ? <Notes/> : <Navigate to="/auth" replace />} />
+      <Route path='/pricing' element={userData ? <Pricing/> : <Navigate to="/auth" replace />} />
     </Routes>
     </>
   )
