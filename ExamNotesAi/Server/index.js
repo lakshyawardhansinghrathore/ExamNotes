8fbd664f6f0b419d9eef786ab61b372fb3279586
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-15 21:50:54 +0530

    add downloadPdf service to call the backend PDF API

--- ExamNotesAi/Server/index.js
+++ ExamNotesAi/Server/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import userRouter from './routes/user.route.js'
 import notesRouter from './routes/generate.route.js'
+import pdfRouter from './routes/pdf.route.js'
 
 dotenv.config()
 
@@ -30,6 +31,7 @@ app.get('/', (req, res) => {
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/notes", notesRouter)
+app.use("/api/pdf", pdfRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
