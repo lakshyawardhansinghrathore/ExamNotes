db798a804c91f8879d48b705d771c532be734cdd
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-16 21:12:54 +0530

    register credit routes and Stripe webhook

--- ExamNotesAi/Server/index.js
+++ ExamNotesAi/Server/index.js
@@ -7,11 +7,19 @@ import cors from 'cors'
 import userRouter from './routes/user.route.js'
 import notesRouter from './routes/generate.route.js'
 import pdfRouter from './routes/pdf.route.js'
+import creditRouter from './routes/credits.route.js'
+import { stripeWebhook } from "./controllers/credits.controller.js";
 
 dotenv.config()
 
 
 const app = express()
+
+app.post(
+    "/api/credits/webhook",
+    express.raw({ type: "application/json" }),
+    stripeWebhook
+)
 
 app.use(cors(
     {origin: "http://localhost:5173",
@@ -32,6 +40,7 @@ app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/notes", notesRouter)
 app.use("/api/pdf", pdfRouter)
+app.use("/api/credit", creditRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
