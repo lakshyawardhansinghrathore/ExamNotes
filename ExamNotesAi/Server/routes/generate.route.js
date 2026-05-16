e0bf7d806b14ff02b85cc6352d3532a7e55540e7
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-05-16 21:07:38 +0530

    .

--- ExamNotesAi/Server/routes/generate.route.js
+++ ExamNotesAi/Server/routes/generate.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import isAuth from '../middleware/isAuth.js';
 import { generateNotes } from '../controllers/generate.controller.js';
-// 👇 Added getSingleNotes to this import!
 import { getMyNotes, getSingleNotes } from '../controllers/notes.controller.js'; 
 
 const notesRouter = express.Router();
